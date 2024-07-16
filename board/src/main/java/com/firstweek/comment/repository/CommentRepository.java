80687c337354de102d6d542a023dd74a95fd77c5
SSAFY_NOTEBOOK <chae0738@naver.com>
2024-07-16 13:18:44 +0900

    init

--- board/src/main/java/com/firstweek/comment/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.firstweek.comment.repository;
-
-import com.firstweek.comment.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-}
--- board/src/main/java/com/firstweek/security/service/AuthService.java
+++
@@ -1,7 +0,0 @@
-package com.firstweek.security.service;
-
-import com.firstweek.security.jwt.TokenPair;
-
-public interface AuthService {
-    TokenPair login(String username);
-}
